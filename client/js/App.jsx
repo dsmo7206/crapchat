feea17ffce129d5c40b8faa8d55f775b1a586eec
David Smoker <dsmoker@gmail.com>
2017-11-14 10:11:58 +0000

    Combined ChatManager class into App class

--- client/js/App.jsx
+++ client/js/App.jsx
@@ -1,11 +1,82 @@
 import React from "react";
-import { ChatManager } from "./ChatManager";
+import { Chat } from "./Chat";
 
 class App extends React.Component {
-    render () {
+    constructor() {
+        super();
+
+        // messageMap maps a chatid (int) to an array of messages (strings)
+        this.state = {messageMap: new Map()};
+
+        this.onSocketMessage = this.onSocketMessage.bind(this);
+        this.createNewChat = this.createNewChat.bind(this);
+        this.requestRefresh = this.requestRefresh.bind(this);
+        this.requestNewMessage = this.requestNewMessage.bind(this);
+    }
+
+    componentDidMount() {
+        this.socket = new WebSocket(
+            ((window.location.protocol === "https:") ? "wss://" : "ws://") + 
+            window.location.host + "/chat"
+        );
+        this.socket.onmessage = this.onSocketMessage;
+
+        this.socket.onopen = () => { 
+            this.createNewChat(123); 
+            this.createNewChat(124); 
+        };
+    }
+
+    onSocketMessage(event) {
+        const data = JSON.parse(event.data);
+        let messageMap = new Map(this.state.messageMap);
+        if (data.type === 'refresh')
+        {
+            messageMap.set(data.chatid, data.data);
+        }
+        else if (data.type === 'new_message')
+        {
+            messageMap.set(data.chatid, messageMap.get(data.chatid).concat(data.data));
+        }
+        this.setState({messageMap: messageMap});
+    }
+
+    createNewChat(chatid) {
+        let messageMap = new Map(this.state.messageMap);
+        messageMap.set(chatid, []);
+        this.setState({messageMap: messageMap});
+    }
+
+    requestRefresh(chatid) {
+        this.socket.send(JSON.stringify({'type': 'refresh', 'chatid': chatid}));
+    }
+
+    requestNewMessage(chatid, message) {
+        this.socket.send(JSON.stringify({
+            'type': 'new_message', 
+            'chatid': chatid, 
+            'message': {'user': 'Web User', 'text': message}
+        }))
+    }
+
+    render() {
+        const messagesArray = Array.from(this.state.messageMap.entries());
+        const chatItems = messagesArray.map((item) => {
+            return (
+                <li key={item[0]}>
+                    <Chat 
+                        chatid={item[0]} 
+                        messages={item[1]} 
+                        requestRefresh={() => {this.requestRefresh(item[0])}}
+                        requestNewMessage={this.requestNewMessage}
+                    />
+                </li>
+            );
+        });
+
         return (
             <div>
-                <ChatManager/>
+                <ul>{chatItems}</ul>
             </div>
         );
     }
